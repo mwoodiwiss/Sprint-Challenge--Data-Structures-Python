e6b9ca3f1a9bbce81ee2b12882b99bdf9d178dcf
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2020-08-30 19:01:12 -0600

    finished-names

--- names/names.py
+++ names/names.py
@@ -10,14 +10,73 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        new_node = BSTNode(value)
+
+        if value < self.value:
+            if self.left is None:
+                self.left = new_node
+            else:
+                self.left.insert(value)
+
+        if value >= self.value:
+            if self.right is None:
+                self.right = new_node
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+
+        elif self.value > target:
+            if self.left is None:
+                return False
+            else:
+                found = self.left.contains(target)
+
+        elif self.value < target:
+            if self.right is None:
+                return False
+            found = self.right.contains(target)
+        return found
+
+    def get_max(self):
+        current = self
+
+        while(current.right is not None):
+            current = current.right
+        return current.value
+
+    def for_each(self, fn):
+        if(self.left):
+            self.left.for_each(fn)
+        fn(self.value)
+        if(self.right):
+            self.right.for_each(fn)
+
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
 
+tree = None
+
+# Replace the nested for loops below with your improvements
+for name in names_1:
+    if tree is None:
+        tree = BSTNode(name)
+        tree.insert(name)
+
+for name in names_2:
+    if tree.contains(name):
+        duplicates.append(name)
+        
+        
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
